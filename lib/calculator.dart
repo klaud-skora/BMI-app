93269c4b1634ad9d37ababd90df319585d61c106
Klaudia Skóra <klaud.skora@gmail.com>
2020-04-01 10:24:33 +0200

    Fix if statement for calculator

--- lib/calculator.dart
+++ lib/calculator.dart
@@ -4,7 +4,7 @@ import 'dart:math';
 String calculator (String heightStr, String weightStr) {
 
   var result ='';
-  if(heightStr != '' && weightStr != '' && (double.tryParse(heightStr) != null || double.tryParse(weightStr) != null) ) {    
+  if(heightStr != '' && weightStr != '' && (double.tryParse(heightStr) != null && double.tryParse(weightStr) != null) ) {    
     var height = int.parse(heightStr);
     var weight = int.parse(weightStr);
     result = (weight / pow(height * 0.01, 2)).toStringAsFixed(2); 
