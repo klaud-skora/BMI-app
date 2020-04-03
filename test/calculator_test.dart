67e28f59ae9289a3413f0beb37e8c51c8e61a8cf
Klaudia Skóra <klaud.skora@gmail.com>
2020-04-04 00:11:14 +0200

    Add tests for imperial calculator

--- test/calculator_test.dart
+++ test/calculator_test.dart
@@ -2,55 +2,110 @@ import 'package:test/test.dart';
 import '../lib/calculator.dart';
 
 void main() {
-Calculator calculator = MetricCalculator();
+Calculator metric = MetricCalculator();
+Calculator imperial = ImperialCalculator();
+
+  // tests for MetricCalculator
 
  test('test good input', () {
-   var result = calculator.calculator(180, 65);
+   var result = metric.calculator(180, 65);
    expect(result, 20.06);
  });
 
   test('test null input for height', () {
-   var result = calculator.calculator(null,60);
+   var result = metric.calculator(null,60);
    expect(result, null);
  });
 
  test('test null input for weight', () {
-   var result = calculator.calculator(180, null);
+   var result = metric.calculator(180, null);
    expect(result, null);
  });
 
  test('test null input for height & weight', () {
-   var result = calculator.calculator(null, null);
+   var result = metric.calculator(null, null);
    expect(result, null);
  });
 
  test('test negative height', () {
-   var result = calculator.calculator(-180, 65);
+   var result = metric.calculator(-180, 65);
    expect(result, null);
  });
 
  test('test negative weight', () {
-   var result = calculator.calculator(180, -65);
+   var result = metric.calculator(180, -65);
    expect(result, null);
  });
 
  test('test negative height & weight', () {
-   var result = calculator.calculator(-180, -65);
+   var result = metric.calculator(-180, -65);
    expect(result, null);
  });
 
  test('test zero height', () {
-   var result = calculator.calculator(0, 65);
+   var result = metric.calculator(0, 65);
    expect(result, null);
  });
 
  test('test zero weight', () {
-   var result = calculator.calculator(180, 0);
+   var result = metric.calculator(180, 0);
    expect(result, null);
  });
 
  test('test zero height & weight', () {
-   var result = calculator.calculator(0, 0);
+   var result = metric.calculator(0, 0);
+   expect(result, null);
+ });
+
+  // tests for ImperialCalculator
+
+  test('test good input', () {
+   var result = imperial.calculator(70.87, 143.3);
+   expect(result, 20.06);
+ });
+
+  test('test null input for height', () {
+   var result = imperial.calculator(null, 143.3);
+   expect(result, null);
+ });
+
+ test('test null input for weight', () {
+   var result = imperial.calculator(70.87, null);
+   expect(result, null);
+ });
+
+ test('test null input for height & weight', () {
+   var result = imperial.calculator(null, null);
+   expect(result, null);
+ });
+
+ test('test negative height', () {
+   var result = imperial.calculator(-70.87, 143.3);
+   expect(result, null);
+ });
+
+ test('test negative weight', () {
+   var result = imperial.calculator(70.87, -143.3);
+   expect(result, null);
+ });
+
+ test('test negative height & weight', () {
+   var result = imperial.calculator(-70.87, -143.3);
+   expect(result, null);
+ });
+
+ test('test zero height', () {
+   var result = imperial.calculator(0, 143.3);
+   expect(result, null);
+ });
+
+ test('test zero weight', () {
+   var result = imperial.calculator(70.87, 0);
+   expect(result, null);
+ });
+
+ test('test zero height & weight', () {
+   var result = imperial.calculator(0, 0);
    expect(result, null);
  });
 
