f026cfffb57183999c02a203b5b422195725ebfa
Klaudia Skóra <klaud.skora@gmail.com>
2020-04-03 23:54:52 +0200

    Add tests for parser

---
+++ test/parser_test.dart
@@ -0,0 +1,24 @@
+import 'package:test/test.dart';
+import '../lib/parser.dart';
+
+void main() {
+  test('test good input' , () {
+    var result = parser('180');
+    expect(result, 180);
+  });
+
+  test('test null input' , () {
+    var result = parser('');
+    expect(result, null);
+  });
+
+  test('test text value' , () {
+    var result = parser('abc');
+    expect(result, null);
+  });
+
+  test('test mixed number & text value' , () {
+    var result = parser('18o');
+    expect(result, null);
+  });
+}
