ead3efbce65891ba12713044924532b1009a106e
Klaudia Skóra <klaud.skora@gmail.com>
2020-04-09 17:29:09 +0200

    Add switch for different colours for BmiIndicator

--- lib/ui/bmi_indicator.dart
+++ lib/ui/bmi_indicator.dart
@@ -9,12 +9,34 @@ class BmiIndicator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Range range = getBMI(bmi);
+    var color;
+
+    switch(range) {
+      case Range.normal:
+        color = Colors.green;
+        break;
+      case Range.underweight:
+      case Range.overweight:
+        color = Colors.yellow;
+        break;
+      case Range.unknown:
+      case Range.obese_I:
+        color = Colors.red[100];
+        break;
+      case Range.obese_II:
+        color = Colors.red[200];
+        break;
+      case Range.obese_III:
+        color = Colors.red[300];
+        break;
+      default:
+        color = Colors.black12;
+    }
+
     return Text(
       '${bmi != null ? bmi : ''}',
       style: TextStyle(
-        color: range == Range.normal ? Colors.green 
-        : (range == Range.underweight || range == Range.overweight ? Colors.yellow 
-        : (range == Range.obese_I  ? Colors.red[100] : range == Range.obese_II ? Colors.red[200] : Colors.red[300] )),
+        color: color,
         shadows: <Shadow>[
           Shadow(
             offset: Offset(.1, .1),
